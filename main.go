440d03566ff54bc04860aca25dc94a8018a91dd6
ssytada <105045839+ssytada@users.noreply.github.com>
2022-05-10 11:56:40 +0900

    Get one data from DB
    Changed the structure type and albumByID so that data can be retrieved from DB.

--- main.go
+++ main.go
@@ -5,48 +5,59 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 	"github.com/go-sql-driver/mysql"
 )
 
 type album struct {
-	ID     string  `json:"id"`
+	ID     int64   `json:"id"`
 	Title  string  `json:"title"`
 	Artist string  `json:"artist"`
-	Price  float64 `json:"price"`
+	Price  float32 `json:"price"`
 }
 
-var albums = []album{
-	{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
-	{ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
-	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
-}
+// var albums = []album{
+// 	{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
+// 	{ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
+// 	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
+// }
 
-func getAlbums(c *gin.Context) {
-	c.IndentedJSON(http.StatusOK, albums)
-}
+// func getAlbums(c *gin.Context) {
+// 	c.IndentedJSON(http.StatusOK, albums)
+// }
 
-func postAlbums(c *gin.Context) {
-	var newAlbum album
+// func postAlbums(c *gin.Context) {
+// 	var newAlbum album
 
-	if err := c.BindJSON(&newAlbum); err != nil {
-		return
-	}
-	albums = append(albums, newAlbum)
-	c.IndentedJSON(http.StatusCreated, newAlbum)
-}
+// 	if err := c.BindJSON(&newAlbum); err != nil {
+// 		return
+// 	}
+// 	albums = append(albums, newAlbum)
+// 	c.IndentedJSON(http.StatusCreated, newAlbum)
+// }
 
 func getAlbumByID(c *gin.Context) {
-	id := c.Param("id")
+	id, _ := strconv.ParseInt(c.Param("id"), 10, 64)
 
-	for _, a := range albums {
-		if a.ID == id {
-			c.IndentedJSON(http.StatusOK, a)
-			return
-		}
+	alb, err := albumByID(id)
+	if err != nil {
+		c.IndentedJSON(http.StatusNotFound, gin.H{"message": "album not found"})
+		return
 	}
-	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "album not found"})
+	c.IndentedJSON(http.StatusOK, alb)
+
+}
+
+func albumByID(id int64) (album, error) {
+	var alb album
+
+	row := db.QueryRow("SELECT * FROM album WHERE id = ?", id)
+	if err := row.Scan(&alb.ID, &alb.Title, &alb.Artist, &alb.Price); err != nil {
+		return alb, fmt.Errorf("albumsById %d: %v", id, err)
+	}
+	return alb, nil
 }
 
 var db *sql.DB
@@ -73,8 +84,8 @@ func main() {
 	fmt.Println("connected!")
 
 	router := gin.Default()
-	router.GET("/albums", getAlbums)
+	// router.GET("/albums", getAlbums)
 	router.GET("/albums/:id", getAlbumByID)
-	router.POST("/albums", postAlbums)
+	// router.POST("/albums", postAlbums)
 	router.Run("localhost:8080")
 }
